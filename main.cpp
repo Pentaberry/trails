d79b3af3a46f01b68857550f16be87e2720a5e7d
Pentaberry <historyafterwards@gmail.com>
2025-07-30 14:10:29 -0400

    did some more stuff
    -Generalized emitters
    -Added a "max emitters" slider
    -Added a new deviation slider, metadeviation, which controls deviation in particle values for trails, including the amount of deviation between individual particles
    -Improved FPS logging

--- main.cpp
+++ main.cpp
@@ -21,6 +21,37 @@ int cam[3];
 int mousepos[2];
 entt::registry particles;
 int frame;
+int alive;
+int frames;
+long long lasttime;
+
+
+static float mdevt; //randomizes particle specifications for each emitter, including devp
+static int maxemits;
+
+static int flamet;
+static float devt; //randomizes base aspects of the emitter: life, physics, and flame
+static int lifet;
+static float speedt;
+static float accxt;
+static float accyt;
+static float acczt;
+static float jrkxt;
+static float jrkyt;
+static float jrkzt;
+
+static float devp; //aka emitter.dev; randomizes values of individual particles
+static int lifep;
+static int sizep;
+static float srgbap[4];
+static float ergbap[4];
+static float speedp;
+static float accxp;
+static float accyp;
+static float acczp;
+static float jrkxp;
+static float jrkyp;
+static float jrkzp;
 
 struct Life {
   int age, lifespan;
@@ -40,15 +71,20 @@ struct Physics {
   float accx, accy, accz;
   float jrkx, jrky, jrkz;
 };
-struct Trail {
-  int min_lifespan, max_lifespan;
-  float x, y, z;
-  int min_size, max_size;
+struct Emitter {
+  int flame;
+  float dev;
+  int life;
+  int size;
   float sr, sg, sb, sa;
   float er, eg, eb, ea;
   float speed;
-  float accx, accy, accz;
-  float jrkx, jrky, jrkz;
+  float accx;
+  float accy;
+  float accz;
+  float jrkx;
+  float jrky;
+  float jrkz;
 };
 
 /* It's gonna be a */ long long time() {
@@ -72,6 +108,12 @@ float randf(float a, float b) {
 int randint(int a, int b) {
   return a + randn(b - a);
 }
+float dev(float n, float d) {
+  return n * randf(1 - d, 1 + d);
+}
+int dev(int n, float d) {
+  return n * randf(1 - d, 1 + d);
+}
 
 int ip(int a, int b, Life life) {
   return a + (b - a) * ((float) life.age / life.lifespan);
@@ -88,29 +130,34 @@ std::array<float, 3> spin(float speed) {
 }
 
 void handle() {
+  ImGuiIO& io = ImGui::GetIO();
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        running = false;
-        break;
-      case SDL_MOUSEWHEEL:
-        cam[2] += event.wheel.y * 80;
-        break;
-      case SDL_MOUSEBUTTONDOWN:
-        pressed = true;
-        break;
-      case SDL_MOUSEBUTTONUP:
-        pressed = false;
-        break;
-      case SDL_MOUSEMOTION:
-        if (pressed) {
-          cam[0] -= event.button.x - mousepos[0];
-          cam[1] -= event.button.y - mousepos[1];
-        }
-        mousepos[0] = event.button.x;
-        mousepos[1] = event.button.y;
-        break;
+    if (io.WantCaptureMouse && !pressed) {
+      ImGui_ImplSDL2_ProcessEvent(&event);
+    } else {
+      switch (event.type) {
+        case SDL_QUIT:
+          running = false;
+          break;
+        case SDL_MOUSEWHEEL:
+          cam[2] += event.wheel.y * 80;
+          break;
+        case SDL_MOUSEBUTTONDOWN:
+          pressed = true;
+          break;
+        case SDL_MOUSEBUTTONUP:
+          pressed = false;
+          break;
+        case SDL_MOUSEMOTION:
+          if (pressed) {
+            cam[0] -= event.button.x - mousepos[0];
+            cam[1] -= event.button.y - mousepos[1];
+          }
+          mousepos[0] = event.button.x;
+          mousepos[1] = event.button.y;
+          break;
+      }
     }
   }
 }
@@ -138,58 +185,48 @@ void update() {
   };
   for (auto particle : dying) {
     particles.destroy(particle);
+    alive--;
   }
 }
 
-void fireworks() {
-  auto vel = spin(randint(10, 20));
+void metaemit() {
+  auto vel = spin(dev(speedt, devt));
   auto trail = particles.create();
-  particles.emplace<Life>(trail, 0, 60);
-  particles.emplace<Position>(trail, 1000, 1000, 1000);
-  particles.emplace<Physics>(trail, vel[0], vel[1], vel[2], 0, 0, 0, 0, 0, 0);
-  particles.emplace<Trail>(trail,
-    100, 125,
-    0, 0, 0,
-    3, 8,
-    0.f, 0.f, 1.f, 1.f,
-    0.f, 1.f, 1.f, 1.f,
-    0.5,
-    0, 0, 0,
-    0, 0.01, 0);
+  alive++;
+  particles.emplace<Life>(trail, 0, dev(lifet, devt));
+  particles.emplace<Position>(trail, randf(500, 1500), randf(500, 1500), randf(500, 1500));
+  particles.emplace<Physics>(trail, vel[0], vel[1], vel[2], dev(accxt, devt), dev(accyt, devt), dev(acczt, devt), dev(jrkxt, devt), dev(jrkyt, devt), dev(jrkzt, devt));
+  particles.emplace<Emitter>(trail, dev(flamet, devt), dev(devp, mdevt), dev(lifep, mdevt), dev(sizep, mdevt), dev(srgbap[0], mdevt), dev(srgbap[1], mdevt), dev(srgbap[2], mdevt), dev(srgbap[3], mdevt), dev(ergbap[0], mdevt), dev(ergbap[1], mdevt), dev(ergbap[2], mdevt), dev(ergbap[3], mdevt), dev(speedp, mdevt), dev(accxp, mdevt), dev(accyp, mdevt), dev(acczp, mdevt), dev(jrkxp, mdevt), dev(jrkyp, mdevt), dev(jrkzp, mdevt));
 }
+    
 
-void dotrails() {
-  bool trailsexist = false;
-  std::vector<Trail> trails;
-  auto view = particles.view<Position, Trail>();
-  for (auto particle : view) {
-    auto& point = view.get<Position>(particle);
-    auto& trail = view.get<Trail>(particle);
-    trail.x = point.x;
-    trail.y = point.y;
-    trail.z = point.z;
-    trails.push_back(trail);
-    trailsexist = true;
-  };
-  for (Trail trail : trails) {
-    for (int i = 0; i < 32; i++) {
-      auto vel = spin(trail.speed);
+void emit() {
+  int emitters = 0;
+  auto view = particles.view<Position, Emitter>();
+  for (auto emitting : view) {
+    emitters++;
+    auto& emitter = view.get<Emitter>(emitting);
+    auto& point = view.get<Position>(emitting);
+    for (int i = 0; i < emitter.flame; i++) {
+      auto vel = spin(dev(emitter.speed, emitter.dev));
       auto particle = particles.create();
-      particles.emplace<Life>(particle, 0, randint(trail.min_lifespan, trail.max_lifespan));
-      particles.emplace<Position>(particle, trail.x, trail.y, trail.z);
-      particles.emplace<Size>(particle, randint(trail.min_size, trail.max_size));
-      particles.emplace<Color>(particle, trail.sr, trail.sg, trail.sb, trail.sa, trail.er, trail.eg, trail.eb, trail.ea);
-      particles.emplace<Physics>(particle, vel[0], vel[1], vel[2], trail.accx, trail.accy, trail.accz, trail.jrkx, trail.jrky, trail.jrkz);
+      float d = randf(1 - emitter.dev, 1 + emitter.dev);
+      alive++;
+      particles.emplace<Life>(particle, 0, dev(emitter.life, emitter.dev));
+      particles.emplace<Position>(particle, point.x, point.y, point.z);
+      particles.emplace<Size>(particle, dev(emitter.size, emitter.dev));
+      particles.emplace<Color>(particle, emitter.sr * d, emitter.sg * d, emitter.sb * d, emitter.sa * d, emitter.er * d, emitter.eg * d, emitter.eb * d, emitter.ea * d);
+      particles.emplace<Physics>(particle, vel[0], vel[1], vel[2], dev(emitter.accx, emitter.dev), dev(emitter.accy, emitter.dev), dev(emitter.accz, emitter.dev), dev(emitter.jrkx, emitter.dev), dev(emitter.jrky, emitter.dev), dev(emitter.jrkz, emitter.dev));
     }
   }
-  if (!trailsexist) {
-    fireworks();
+  if (emitters < maxemits) {
+    metaemit();
   }
 }
 
 void process() {
   update();
-  dotrails();
+  emit();
 }
 
 void render() {
@@ -225,12 +262,54 @@ void render() {
     }
   }
   
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplSDL2_NewFrame();
+  ImGui::NewFrame();
+  
+  ImGui::SetNextWindowSize(ImVec2(350, 350), ImGuiCond_Once);
+  ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Once);
+  ImGui::Begin("Emitter Controls");
+  ImGui::SliderInt("Max Emitters", &maxemits, 1, 100);
+  ImGui::SliderFloat("Meta-Deviation", &mdevt, 0.f, 1.f);
+  ImGui::SliderFloat("Deviation", &devt, 0.f, 1.f);
+  ImGui::SliderInt("Lifespan", &lifet, 30, 120);
+  ImGui::SliderInt("Flame", &flamet, 0, 50);
+  ImGui::SliderFloat("Speed", &speedt, 0.f, 2.f);
+  ImGui::SliderFloat("Acc X", &accxt, -0.2f, 0.2f);
+  ImGui::SliderFloat("Acc Y", &accyt, -0.2f, 0.2f);
+  ImGui::SliderFloat("Acc Z", &acczt, -0.2f, 0.2f);
+  ImGui::SliderFloat("Jerk X", &jrkxt, -0.1f, 0.1f);
+  ImGui::SliderFloat("Jerk Y", &jrkyt, -0.1f, 0.1f);
+  ImGui::SliderFloat("Jerk Z", &jrkzt, -0.1f, 0.1f);
+  ImGui::End();
+
+  ImGui::SetNextWindowSize(ImVec2(300, 800), ImGuiCond_Once);
+  ImGui::SetNextWindowPos(ImVec2(700, 0), ImGuiCond_Once);
+  ImGui::Begin("Particle Controls");
+  ImGui::SliderFloat("Deviation", &devp, 0.f, 1.f);
+  ImGui::SliderInt("Lifespan", &lifep, 30, 120);
+  ImGui::SliderInt("Size", &sizep, 1, 10);
+  ImGui::SliderFloat("Speed", &speedp, 0.f, 2.f);
+  ImGui::SliderFloat("Acc X", &accxp, -0.2f, 0.2f);
+  ImGui::SliderFloat("Acc Y", &accyp, -0.2f, 0.2f);
+  ImGui::SliderFloat("Acc Z", &acczp, -0.2f, 0.2f);
+  ImGui::SliderFloat("Jerk X", &jrkxp, -0.1f, 0.1f);
+  ImGui::SliderFloat("Jerk Y", &jrkyp, -0.1f, 0.1f);
+  ImGui::SliderFloat("Jerk Z", &jrkzp, -0.1f, 0.1f);
+  ImGui::ColorPicker4("Start Color", srgbap);
+  ImGui::ColorPicker4("End Color", ergbap);
+  ImGui::End();
+  
+  ImGui::Render();
+  
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
   SDL_GL_SwapWindow(window);
 } 
 
 void stars() {
-  for (int i = 0; i < pow(2, 11); i++) {
+  for (int i = 0; i < pow(2, 10); i++) {
     auto star = particles.create();
+    alive++;
     particles.emplace<Life>(star, 0, 1000000000);
     particles.emplace<Position>(star, rand01() * 2000, rand01() * 2000, rand01() * 2000);
     particles.emplace<Size>(star, randint(1, 5));
@@ -241,39 +320,69 @@ void stars() {
 
 void init() {
   SDL_Init(SDL_INIT_VIDEO);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
   window = SDL_CreateWindow("Trails", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_OPENGL);
   context = SDL_GL_CreateContext(window);
   SDL_GL_MakeCurrent(window, context);
   SDL_GL_SetSwapInterval(1);
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
-  auto io = ImGui::GetIO();
-  (void) io;
   ImGui::StyleColorsDark();
   ImGui_ImplSDL2_InitForOpenGL(window, context);
   ImGui_ImplOpenGL3_Init("#version 130");
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+  maxemits = 3;
+  mdevt = 1;
+  devt = 0.125;
+  lifet = 60;
+  speedt = 16;
+  accxt = 0;
+  accyt = 0;
+  acczt = 0;
+  jrkxt = 0;
+  jrkyt = 0;
+  jrkzt = 0;
+  flamet = 10;
+
+  devp = 0.125;
+  lifep = 120;
+  sizep = 6;
+  srgbap[0] = 0; srgbap[1] = 0; srgbap[2] = 1; srgbap[3] = 1; //blue
+  ergbap[0] = 1; ergbap[1] = 1; ergbap[2] = 0; ergbap[3] = 1; //yellow
+  speedp = 0.5;
+  accxp = 0;
+  accyp = 0;
+  acczp = 0;
+  jrkxp = 0;
+  jrkyp = -0.01;
+  jrkzp = 0;
+
   running = true;
   pressed = false;
-  cam[0] = 1000; cam[1] = 1000; cam[2] = 0;
-  frame = 0;
+  cam[0] = 1000; cam[1] = 1000; cam[2] = -2000;
+  frames = 0;
+  lasttime = time();
+  alive = 0;
   srand(time(0));
 }
 
 void loop() {
-  long long start = time();
   handle();
   process();
   render();
-  int timepassed = time() - start;
-  if (frame % 60 == 0) {
-    std::cout << "FPS: " << (timepassed == 0 ? "∞" : std::to_string(1000 / timepassed)) << std::endl;
+  frames++;
+  long long span = (time() - lasttime) / 1000;
+  if (span > 1) {
+    std::cout << "FPS: " << (frames / span) << std::endl;
+    std::cout << "Particles: " << alive << std::endl;
+    frames = 0;
+    lasttime = time();
   }
   
-  std::this_thread::sleep_for(std::chrono::milliseconds(std::max(0, (int) (1000.f / 60 - timepassed))));
-  frame++;
+  //std::this_thread::sleep_for(std::chrono::milliseconds(std::max(0, (int) (1000.f / 60 - timepassed))));
 }
 
 void close() {
